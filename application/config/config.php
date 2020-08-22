83d191428921d9140dd77be9dc4d5eeaaea4c268
Zephaniah Langley <millennialbusinessbuilders@gmail.com>
2020-08-22 11:02:25 -0400

    updated congif base url

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
 $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-$config['base_url'] = $root;//'https://getdinerotodayaccounting.herokuapp.com/';//$root;
+$config['base_url'] = 'https://test-heroku-ci-mysql.herokuapp.com/';//$root;
 die($config['base_url']);
 /*
 |--------------------------------------------------------------------------
